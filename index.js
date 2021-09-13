ef1ee984933032b26c8cc25010743f3a53683490
jingyi1203 <90587365+jingyi1203@users.noreply.github.com>
2021-09-13 07:00:15 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+let btn = document.getElementsByClassName('btn')[0],
+    mask = document.getElementsByClassName('mask')[0],
+    nav = document.getElementsByClassName('nav')[0];
+
+btn.addEventListener('click', function() {
+   mask.style.display = 'block';
+   nav.style.left = '0';
+}, false);
+
+mask.addEventListener('click', function() {
+   mask.style.display = 'none';
+   nav.style.left = '-30rem';
+}, false)
