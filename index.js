f1fcf7d7567e4a8212b47a7306be482dd3444800
jingyi1203 <90587365+jingyi1203@users.noreply.github.com>
2021-09-13 09:26:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,10 @@ let btn = document.getElementsByClassName('btn')[0],
     mask = document.getElementsByClassName('mask')[0],
     nav = document.getElementsByClassName('nav')[0];
 
-function showNav(){
-    mask.fadeIn();
-    nav.animate({'right':0});
+function openNav() {
+    nav.style.width = "250px";
 }
 
-function hideNav(){
-    mask.fadeOut();
-    nav.css('right',-nav.width());
-    console.log("mask");
+function closeNav() {
+    nav.style.width = "0";
 }
-
-btn.on('click',showNav);
-mask.click(hideNav);
