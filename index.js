6438030d75c883c4860860c076a70f246d218491
jingyi1203 <90587365+jingyi1203@users.noreply.github.com>
2021-09-13 09:38:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-let btn = document.getElementsByClassName('btn')[0],
-    mask = document.getElementsByClassName('mask')[0],
-    nav = document.getElementsByClassName('nav')[0];
 
 function openNav() {
-    nav.style.width = "250px";
+    document.getElementById("mySidenav").style.width = "250px";
 }
 
 function closeNav() {
-    nav.style.width = "0";
+    document.getElementById("mySidenav").style.width = "0";
 }
