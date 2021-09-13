6f4c8ff470ed1a2d30d339b1651fad7e48d4ad45
jingyi1203 <90587365+jingyi1203@users.noreply.github.com>
2021-09-13 07:46:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ let btn = document.getElementsByClassName('btn')[0],
     mask = document.getElementsByClassName('mask')[0],
     nav = document.getElementsByClassName('nav')[0];
 
+window.onload = function(){
+    btn.onmouseover = function(){
+        startMove(0);
+    }
+    btn.onmouseover = function(){
+        startMove(-30);
+    }
+}
+
+var timer = null;
+function startMove(iTargat){
+    clearInterval(timer);
+
 btn.addEventListener('click', function() {
    mask.style.display = 'block';
    nav.style.left = '0';
